e83b0b0c281b453b355b80968170642bfde4b0bd
Andruraj <aamalraj@plexustg.com>
2023-02-05 18:50:19 +0530

    Simple Tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,21 @@
-import { Button } from "./components/AdvancedButton";
+import { Tooltip } from "./components/Tooltip";
 
 export default function App() {
   return (
     <div className="h-screen w-full">
-      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <Button className="m-1" rounded variant="primary">
-          primary
-        </Button>
-        <Button className="m-1" rounded variant="secondary">
-          secondary
-        </Button>
-        <Button className="m-1" rounded variant="success">
-          success
-        </Button>
-        <Button className="m-1" rounded variant="danger">
-          danger
-        </Button>
-        <Button className="m-1" rounded variant="warning">
-          warning
-        </Button>
-        <Button className="m-1" rounded variant="info">
-          info
-        </Button>
-        <Button className="m-1" rounded variant="light">
-          light
-        </Button>
-        <Button className="m-1" rounded variant="dark">
-          dark
-        </Button>
-        <Button className="m-1" rounded variant="primary-outline">
-          primary-outline
-        </Button>
-        <Button className="m-1" rounded variant="secondary-outline">
-          secondary-outline
-        </Button>
-        <Button className="m-1" rounded variant="success-outline">
-          success-outline
-        </Button>
-        <Button className="m-1" rounded variant="danger-outline">
-          danger-outline
-        </Button>
-        <Button className="m-1" rounded variant="warning-outline">
-          warning-outline
-        </Button>
-        <Button className="m-1" rounded variant="info-outline">
-          info-outline
-        </Button>
-        <Button className="m-1" rounded variant="light-outline">
-          light-outline
-        </Button>
-        <Button className="m-1" rounded variant="dark-outline">
-          dark-outline
-        </Button>
-        <Button className="m-1" disabled>
-          disabled
-        </Button>
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 flex flex-col items-center justify-center">
+        <Tooltip position="top" content="This is Tooltip on top">
+          Tooltip-top
+        </Tooltip>
+        <Tooltip position="right" content="This is Tooltip on right">
+          Tooltip-right
+        </Tooltip>
+        <Tooltip position="left" content="This is Tooltip on left">
+          Tooltip-left
+        </Tooltip>
+        <Tooltip position="bottom" content="This is Tooltip on bottom">
+          Tooltip-bottom
+        </Tooltip>
       </div>
     </div>
   );
---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,50 @@
+import PropTypes from "prop-types";
+import { classNames } from "./classNames";
+
+export const Tooltip = ({ position, content, children }) => (
+  <div id="tooltip" className="relative cursor-pointer group">
+    <div className="mx-2 my-1">{children}</div>
+    <span
+      className={classNames(
+        "absolute inline-block bg-neutral-900 text-white text-xs p-2 whitespace-nowrap rounded",
+        position === "top"
+          ? "left-1/2 -translate-x-1/2 bottom-[calc(100%+5px)]"
+          : "",
+        position === "bottom"
+          ? "left-1/2 -translate-x-1/2 top-[calc(100%+5px)]"
+          : "",
+        position === "left"
+          ? "top-1/2 -translate-y-1/2 right-[calc(100%+5px)]"
+          : "",
+        position === "right"
+          ? "top-1/2 -translate-y-1/2 left-[calc(100%+5px)]"
+          : ""
+      )}
+    >
+      {content}
+    </span>
+    <span
+      className={classNames(
+        "absolute inline-block border-[6px]",
+        position === "top"
+          ? "left-1/2 -translate-x-1/2 bottom-full border-l-transparent border-r-transparent border-b-0 border-t-neutral-900"
+          : "",
+        position === "bottom"
+          ? "left-1/2 -translate-x-1/2 top-full border-l-transparent border-r-transparent border-t-0 border-b-neutral-900"
+          : "",
+        position === "left"
+          ? "top-1/2 -translate-y-1/2 right-full border-t-transparent border-b-transparent border-r-0 border-l-neutral-900"
+          : "",
+        position === "right"
+          ? "top-1/2 -translate-y-1/2 left-full border-t-transparent border-b-transparent border-l-0 border-r-neutral-900"
+          : ""
+      )}
+    ></span>
+  </div>
+);
+
+Tooltip.propTypes = {
+  position: PropTypes.oneOf(["top", "bottom", "left", "right"]).isRequired,
+  content: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
+};
